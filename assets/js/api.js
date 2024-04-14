8b01d5622538bf686b8d0f431964af93fecd8d44
Trilochan Patra <144446993+Franky1250@users.noreply.github.com>
2024-04-14 11:48:52 +0530

    All required Files are added

---
+++ assets/js/api.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const api_key = '2445969dcaf5fbdec5e6a1fcdfbe18ed';
+const imageBaseURL = 'https://image.tmdb.org/t/p/';
+
+/**
+ * fetch data from a server using the `url` and passes
+ * the result in JSON data to a `callback` function,
+ * along with an optional parameter if has `optionalParam`.
+ */
+
+const fetchDataFromServer = function (url, callback, optionalParam) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => callback(data, optionalParam));
+}
+
+export { imageBaseURL, api_key, fetchDataFromServer };
